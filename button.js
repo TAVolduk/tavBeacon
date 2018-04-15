571181aace7232d07517e446ef54232ffb3e6d69
Hamza TAS <hamzalitas95@gmail.com>
2018-04-15 06:34:48 +0300

    button and socket merged

--- button.js
+++ button.js
@@ -1,18 +1,24 @@
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
 
-var pushButton = new Gpio(4, 'in', 'rising', {debounceTimeout: 10});
+module.exports = function onButtonPushed(callback) {
+  var pushButton = new Gpio(4, "in", "rising", { debounceTimeout: 10 });
 
-pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
-  if (err) { //if an error
-    console.error('There was an error', err); //output error message to console
-  return;
+  pushButton.watch(function(err, value) {
+    //Watch for hardware interrupts on pushButton GPIO, specify callback function
+    if (err) {
+      //if an error
+      console.error("There was an error", err); //output error message to console
+      return;
+    }
+    // console.log("sombody pushed my button!");
+    if (callback && typeof callback === "function") callback();
+  });
+
+  function unexportOnClose() {
+    //function to run when exiting program
+    console.log("close");
+    pushButton.unexport(); // Unexport Button GPIO to free resources
   }
-  console.log("sombody pushed my button!");
-});
 
-function unexportOnClose() { //function to run when exiting program
-  console.log("close");
-  pushButton.unexport(); // Unexport Button GPIO to free resources
+  process.on("SIGINT", unexportOnClose); //function to run when user closes using ctrl+c
 };
-
-process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c 
--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const noble = require("noble");
+const io = require("socket.io-client");
+const onPushButton = require("./button");
+
+// const networkInterface = require('os').networkInterfaces();
+const macaddress = "luggagemacadr";
 
 const logError = err => console.log(err);
 
-noble.on('stateChange', (state) => {
-  if (state === 'poweredOn') {
-    noble.startScanning([], true, (err) => {
-      if (err) {
-        return console.log(err);
-      }
+const socket = io("http://localhost:1337/checkLuggage");
 
-      console.log("scanning...");
-    });
-  } else {
-    noble.stopScanning(logError);
-  }
-})
-
-
-noble.on('discover', (peripheral) => {
-  if (peripheral.address === "e5:fe:c1:fa:b5:e6") {
-    console.log("RSSI: " + peripheral.rssi);
-  }
+onPushButton(() => {
+  socket.emit("fetch_data", macaddress);
 });
+
+socket.on("flight_data", function (data) {
+  noble.on('stateChange', (state) => {
+    if (state === 'poweredOn') {
+      noble.startScanning([], true, (err) => {
+        if (err) {
+          return console.log(err);
+        }
+
+        console.log("scanning...");
+      });
+    } else {
+      noble.stopScanning(logError);
+    }
+  })
+
+
+  noble.on('discover', (peripheral) => {
+    if (peripheral.address === "e5:fe:c1:fa:b5:e6") {
+      console.log("RSSI: " + peripheral.rssi);
+    }
+  });
+});
+
 
 
